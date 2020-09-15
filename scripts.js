85df5eff05c011a735d4308ac3aa7c05e986ae5f
rodrigo <rodrigo.rgtic@gmail.com>
2020-09-14 21:44:42 -0300

    exemplo de lista

--- scripts.js
+++ scripts.js
@@ -11,14 +11,28 @@ function RegressiveCounter(value){
       document.getElementById('image').src = './img/tetra.gif';
     }
 
-  }, 1000);
-  
+  }, 1000);  
 }
 
 
 document.getElementById('button').addEventListener('click', function(){  
   const timerValue = document.getElementById('input').value;
   RegressiveCounter(timerValue);
+});
+
+
+
+
+document.getElementById('add-name').addEventListener('click', function(){  
+ 
+  const name = document.getElementById('input-name').value;
+
+  // Exemplo de como adicionar um item na lista.
+  const lista = document.getElementById('name-list');
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(name));
+  lista.appendChild(li);
+  
 });
 
 
